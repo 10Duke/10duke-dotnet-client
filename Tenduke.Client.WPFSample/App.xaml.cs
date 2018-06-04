ad3d07d4826cc643e7add39999989539f12e5e48
Jarkko Selkäinaho <jarkko@10duke.com>
2018-06-04 17:14:32 +0300

    WPF sample app, work in progress

---
+++ Tenduke.Client.WPFSample/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Tenduke.Client.WPF;
+
+namespace Tenduke.Client.WPFSample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            EntClient.Initialize();
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            EntClient.Shutdown();
+        }
+    }
+}
