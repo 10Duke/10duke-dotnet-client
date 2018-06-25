c5e5e4c62bc90d0df4c297dd825d19fa221cec0a
Jarkko Selkäinaho <jarkko@10duke.com>
2018-06-25 15:33:59 +0300

    WPF client and sample implementation, work in progress

--- Tenduke.Client.WPF/WebBrowserWindow.xaml.cs
+++ Tenduke.Client.WPF/WebBrowserWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using CefSharp.Wpf;
+﻿using CefSharp;
+using CefSharp.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -51,6 +53,137 @@ namespace Tenduke.Client.WPF
         public WebBrowserWindow()
         {
             InitializeComponent();
+        }
+
+        #endregion
+
+        #region Private methods
+
+        /// <summary>
+        /// Called when the web browser window is shown.
+        /// </summary>
+        /// <param name="sender">Sender of the event.</param>
+        /// <param name="e">The event arguments.</param>
+        private void windowWebBrowser_Loaded(object sender, RoutedEventArgs e)
+        {
+            ChromiumWebBrowser = new ChromiumWebBrowser
+            {
+                Address = Address,
+                RequestHandler = new AuthzRequestHandler(this)
+            };
+            gridForWebBrowser.Children.Add(ChromiumWebBrowser);
+        }
+
+        /// <summary>
+        /// Called from <see cref="IRequestHandler.OnProtocolExecution(IWebBrowser, IBrowser, string)"/> of the browser request handler.
+        /// Allows this component to intercept OAuth response callbacks.
+        /// </summary>
+        /// <param name="browserControl">The browser control.</param>
+        /// <param name="browser">The browser.</param>
+        /// <param name="url">The redirect URL used for sending the OAuth response.</param>
+        /// <returns>Always returns <c>false</c>, never letting the browser continue handling after this handler.</returns>
+        private bool HandleBrowserProtocolExecution(IWebBrowser browserControl, IBrowser browser, string url)
+        {
+            if (url.StartsWith(RedirectUri))
+            {
+                ResponseUri = url;
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    // true indicates that response has been received, no matter
+                    // if the response contains an OAuth success response or an error response
+                    DialogResult = true;
+                    Close();
+                }));
+            }
+
+            return false;
+        }
+
+        #endregion
+
+        #region Private CefSharp request handler implementation
+
+        /// <summary>
+        /// Implementation of CefSharp request handler, used for capturing the OAuth response.
+        /// </summary>
+        private class AuthzRequestHandler : IRequestHandler
+        {
+            private readonly WebBrowserWindow parent;
+
+            public AuthzRequestHandler(WebBrowserWindow parent)
+            {
+                this.parent = parent;
+            }
+
+            public bool GetAuthCredentials(IWebBrowser browserControl, IBrowser browser, IFrame frame, bool isProxy, string host, int port, string realm, string scheme, IAuthCallback callback)
+            {
+                return false;
+            }
+
+            public IResponseFilter GetResourceResponseFilter(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, IResponse response)
+            {
+                return null;
+            }
+
+            public bool OnBeforeBrowse(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, bool isRedirect)
+            {
+                return false;
+            }
+
+            public CefReturnValue OnBeforeResourceLoad(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, IRequestCallback callback)
+            {
+                return CefReturnValue.Continue;
+            }
+
+            public bool OnCertificateError(IWebBrowser browserControl, IBrowser browser, CefErrorCode errorCode, string requestUrl, ISslInfo sslInfo, IRequestCallback callback)
+            {
+                return false;
+            }
+
+            public bool OnOpenUrlFromTab(IWebBrowser browserControl, IBrowser browser, IFrame frame, string targetUrl, WindowOpenDisposition targetDisposition, bool userGesture)
+            {
+                return true;
+            }
+
+            public void OnPluginCrashed(IWebBrowser browserControl, IBrowser browser, string pluginPath)
+            {
+            }
+
+            public bool OnProtocolExecution(IWebBrowser browserControl, IBrowser browser, string url)
+            {
+                return parent.HandleBrowserProtocolExecution(browserControl, browser, url);
+            }
+
+            public bool OnQuotaRequest(IWebBrowser browserControl, IBrowser browser, string originUrl, long newSize, IRequestCallback callback)
+            {
+                return false;
+            }
+
+            public void OnRenderProcessTerminated(IWebBrowser browserControl, IBrowser browser, CefTerminationStatus status)
+            {
+            }
+
+            public void OnRenderViewReady(IWebBrowser browserControl, IBrowser browser)
+            {
+            }
+
+            public void OnResourceLoadComplete(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, IResponse response, UrlRequestStatus status, long receivedContentLength)
+            {
+            }
+
+            public void OnResourceRedirect(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, IResponse response, ref string newUrl)
+            {
+            }
+
+            public bool OnResourceResponse(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, IResponse response)
+            {
+                return false;
+            }
+
+            public bool OnSelectClientCertificate(IWebBrowser browserControl, IBrowser browser, bool isProxy, string host, int port, X509Certificate2Collection certificates, ISelectClientCertificateCallback callback)
+            {
+                return false;
+            }
         }
 
         #endregion
--- Tenduke.Client.WPFSample/Properties/Settings.Designer.cs
+++ Tenduke.Client.WPFSample/Properties/Settings.Designer.cs
@@ -8,22 +8,107 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Tenduke.Client.WPFSample.Properties
-{
-
-
+namespace Tenduke.Client.WPFSample.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("https://dominos-poc.10duke.com/oauth2/authz/")]
+        public string AuthzUri {
+            get {
+                return ((string)(this["AuthzUri"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("https://dominos-poc.10duke.com/oauth2/access/")]
+        public string TokenUri {
+            get {
+                return ((string)(this["TokenUri"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("https://dominos-poc.10duke.com/userinfo/")]
+        public string UserInfoUri {
+            get {
+                return ((string)(this["UserInfoUri"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("SampleApp")]
+        public string ClientID {
+            get {
+                return ((string)(this["ClientID"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("YohSi-u3")]
+        public string ClientSecret {
+            get {
+                return ((string)(this["ClientSecret"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("oob:SampleApp")]
+        public string RedirectUri {
+            get {
+                return ((string)(this["RedirectUri"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("openid profile email")]
+        public string Scope {
+            get {
+                return ((string)(this["Scope"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute(@"-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1wRc5dsWBbIJfxay3SYP
+MYp/BaLEt0b26/QtwQbrKq6hgVH+euMWsSk6gf0GZiwHMFF+t8/WcsNOfcYMBEHV
+mGWSFeYb63IcFN+v3h2580kANzuKuqYnBeBOCN56lJf8q5FOUYQKFVuX/bvEKp+L
+1KkMErmIm9e5fkw70zCngxoXGK6qyWX01SPTVfd3UZdPv1H+VOoEpbDsI2yhg5xR
+jFAAsqyTYvHQaixiJqqw/T8+2/ond8AlxpzCa1UK9x2l1lMezlwHTHXyPh2ZMpwe
+lDBIosKLPHbaZyNwpU0iGOvrDJo8xlw4qGm/fClbaEWM8BCdbn/aKjWMN/t7FEaQ
+TQIDAQAB
+-----END PUBLIC KEY-----")]
+        public string SignerKey {
+            get {
+                return ((string)(this["SignerKey"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool StoreAuthorization {
+            get {
+                return ((bool)(this["StoreAuthorization"]));
             }
         }
     }
