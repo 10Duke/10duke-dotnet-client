7319119f6a7911f16b678e1a428c3c2252c5a75f
jarkko <jarkko@10duke.com>
2019-03-19 11:04:46 +0200

    Add Tenduke.Client/EntClient for making API calls when OAuth access token is received from external location

--- Tenduke.Client/Config/OAuthConfig.cs
+++ Tenduke.Client/Config/OAuthConfig.cs
@@ -7,7 +7,7 @@ namespace Tenduke.Client.Config
     /// Base class OAuth configuration objects.
     /// </summary>
     [Serializable]
-    public abstract class OAuthConfig : IOAuthConfig, ISerializable
+    public class OAuthConfig : IOAuthConfig, ISerializable
     {
         #region Properties
 
---
+++ Tenduke.Client/EntClient.cs
@@ -0,0 +1,17 @@
+﻿using Tenduke.Client.Config;
+
+namespace Tenduke.Client
+{
+    /// <summary>
+    /// Client for calling APIs of the 10Duke Identity and Entitlement Service, specifically the
+    /// <see cref="Tenduke.Client.EntApi.Authz.AuthzApi"/> and the <see cref="Tenduke.Client.UserInfo.UserInfoApi"/>.
+    /// The API calls are authenticated and authorized by OAuth 2.0, i.e. <see cref="BaseClient{C, A}.AccessToken"/>
+    /// must always be set for making API calls. 10Duke provides other platform specific client implementations
+    /// derived from <see cref="BaseClient{C, A}"/> that also implement OAuth 2.0 authorization flow for acquiring
+    /// the access token. This client implementation can be used when the access token is acquired using a custom
+    /// process external to the client use case, or when no specific client implementation is available.
+    /// </summary>
+    public class EntClient : BaseClient<EntClient, OAuthConfig>
+    {
+    }
+}
