1e7d06a3c0dde2d4629eb4888b902dff0c93f2f7
Jarkko S <jarkko@10duke.com>
2019-03-21 09:24:21 +0200

    Improve test

--- Tenduke.Client.Test/EntApiClientTest.cs
+++ Tenduke.Client.Test/EntApiClientTest.cs
@@ -11,7 +11,16 @@ namespace Tenduke.Client
         private readonly string accessToken = "insert-valid-oauth-access-token-value-here";
         private readonly string userInfoUri = "https://my-ent.10duke.net/userinfo";
         private readonly string authzUri = "https://my-ent.10duke.net/authz/";
-        private readonly string tokenSignerKey = @"Insert PEM / PKCS#1 encoded RSA key here";
+        // Replace this with the environment specific PEM / PKCS#1 encoded RSA key
+        private readonly string tokenSignerKey = @"-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxSjO1Dmx3ubqPl13Ch60
+K9qiG0xyuEUSes2ZhzvwsEHu3q5vTuewNzFqZe3g67lz5zfbzjAkcMKMJ6CjxwgL
+b2Zt70DOuXUNT7o6JJ9H7HycNAnMmX2Luhv/9JrULNqdMpO0n1ncEsZVcW/cZH+t
+tFT2ssc7kISdokLJdDp4CEhUEXu23Zzyxw+vmOUpbYxsUIw4nmhLihxRtTP5WwhY
+smbVXi0S3sbSrkUyisotf94ZMSbiFb5gnYEO4kc5FlyXET8nqL1ck++l5oyK1tLO
+bPqW6l/8gsDUzpf6hQaQP3/NTLFX4T3sBSkX55MDtnEnQePwylfJXxmKGkjapCeX
+FQIDAQAB
+-----END PUBLIC KEY-----";
 
         [Test]
         [Ignore("UserInfoUri and AccessToken must be configured for running the test")]
@@ -34,7 +43,7 @@ namespace Tenduke.Client
         }
 
         [Test]
-        [Ignore("SignerKey, EndpointUri and AccessToken must be configured for running the test")]
+        //[Ignore("SignerKey, EndpointUri and AccessToken must be configured for running the test")]
         public async Task TestAuthzApi()
         {
             var signerKey = CryptoUtil.ReadRsaPublicKey(tokenSignerKey);
@@ -50,9 +59,14 @@ namespace Tenduke.Client
                 AccessToken = accessToken
             };
 
-            var authorizationDecision = await client.AuthzApi.CheckOrConsumeAsync("MyTestItem", false, ResponseType.JWT);
+            var authzApi = client.AuthzApi;
+            var computerId = "MyComputerId";
+            authzApi.ComputerId = computerId;
+
+            var authorizationDecision = await authzApi.CheckOrConsumeAsync("MyTestItem", false, ResponseType.JWT);
 
             Assert.That(authorizationDecision["iss"], Is.Not.Null.And.Not.Empty);
+            Assert.That(authzApi.ComputerId, Is.EqualTo(computerId));
         }
     }
 }
