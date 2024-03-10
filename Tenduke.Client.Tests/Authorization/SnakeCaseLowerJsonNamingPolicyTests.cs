b96001ba4b23201c083accfd1eacaa691d8e31b0
Hamish Smith <hamish.smith@10duke.com>
2024-03-11 11:27:55 +1300

    Use System.Text.Json to deserialize access token
    System.Text.Json is faster than NewtonSoft and is part of the .NET
    project.
    Over time, it would be good to move to using System.Text.Json for all
    JSON serialization and deserialization needs, and drop NewtonSoft.Json
    as a dependency.

---
+++ Tenduke.Client.Tests/Authorization/SnakeCaseLowerJsonNamingPolicyTests.cs
@@ -0,0 +1,25 @@
+﻿using Tenduke.Client.Authorization;
+namespace Tenduke.Client.Tests.Authorization;
+
+public class SnakeCaseLowerJsonNamingPolicyTests
+{
+    [Theory]
+    [InlineData("AccessToken", "access_token")]
+    [InlineData("TokenType", "token_type")]
+    [InlineData("ExpiresIn", "expires_in")]
+    [InlineData("RefreshToken", "refresh_token")]
+    [InlineData("Scope", "scope")]
+    [InlineData("IdToken", "id_token")]
+    [InlineData("Remember", "remember")]
+    [InlineData("Error", "error")]
+    [InlineData("ErrorDescription", "error_description")]
+    [InlineData("ErrorUri", "error_uri")]
+    public void ConvertNameConvertsIdTokenFieldsToExpectedValues(string input, string expected)
+    {
+        var converter = new SnakeCaseLowerJsonNamingPolicy();
+
+        var result = converter.ConvertName(input);
+
+        Assert.Equal(expected, result);
+    }
+}
